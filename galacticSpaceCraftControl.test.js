5ba66fec86226a8b1aa2e2c3b8b48dea6d0c0a17
heyhiru <hirentimbadiya74@gmail.com>
2023-08-30 00:53:41 +0530

    added unit tests for spacecraft movements in north direction
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

--- galacticSpaceCraftControl.test.js
+++ galacticSpaceCraftControl.test.js
@@ -17,3 +17,36 @@ function checkspaceCraftState(spaceCraft, x, y, z, direction) {
 
     expect(DIRECTIONS[spaceCraft.directionIndex]).toBe(direction);
 }
+
+// unit tests for spacecraft movements in NORTH direction
+describe("spaceCraft movements in North direction", () => {
+    test("moves spaceCraft forward when direction is North", () => {
+        const spaceCraft = initializeAndExecute(0, 0, 0, "N", "f");
+        checkspaceCraftState(spaceCraft, 0, 1, 0, "N");
+    });
+
+    test("moves spaceCraft backward when direction is North", () => {
+        const spaceCraft = initializeAndExecute(0, 0, 0, "N", "b");
+        checkspaceCraftState(spaceCraft, 0, -1, 0, "N");
+    });
+
+    test("turns spaceCraft left from North direction", () => {
+        const spaceCraft = initializeAndExecute(0, 0, 0, "N", "l");
+        checkspaceCraftState(spaceCraft, 0, 0, 0, "W");
+    });
+
+    test("turns spaceCraft right from North direction", () => {
+        const spaceCraft = initializeAndExecute(0, 0, 0, "N", "r");
+        checkspaceCraftState(spaceCraft, 0, 0, 0, "E");
+    });
+
+    test("turns spaceCraft up from North direction", () => {
+        const spaceCraft = initializeAndExecute(0, 0, 0, "N", "u");
+        checkspaceCraftState(spaceCraft, 0, 0, 0, "U");
+    });
+
+    test("turns spaceCraft down from North direction", () => {
+        const spaceCraft = initializeAndExecute(0, 0, 0, "N", "d");
+        checkspaceCraftState(spaceCraft, 0, 0, 0, "D");
+    });
+});

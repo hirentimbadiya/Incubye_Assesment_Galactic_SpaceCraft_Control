8814e7b60b9584232b39919eee9b5c383bddcddb
heyhiru <hirentimbadiya74@gmail.com>
2023-08-30 00:58:30 +0530

    implemented space craft control methods
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

--- galacticSpaceCraftControl.js
+++ galacticSpaceCraftControl.js
@@ -9,4 +9,100 @@ class GalacticSpaceCraftControl {
 
         this.directionIndex = DIRECTIONS.indexOf(direction);
     }
+
+    moveForward() {
+        switch (DIRECTIONS[this.directionIndex]) {
+            case 'N':
+                this.position[1]++;
+                break;
+            case 'E':
+                this.position[0]++;
+                break;
+            case 'S':
+                this.position[1]--;
+                break;
+            case 'W':
+                this.position[0]--;
+                break;
+            case 'U':
+                this.position[2]++;
+                break;
+            case 'D':
+                this.position[2]--;
+                break;
+            default:
+                throw new Error(`Invalid direction`);
+
+        }
+    }
+
+    moveBackward() {
+        switch (DIRECTIONS[this.directionIndex]) {
+            case 'N':
+                this.position[1]--;
+                break;
+            case 'E':
+                this.position[0]--;
+                break;
+            case 'S':
+                this.position[1]++;
+                break;
+            case 'W':
+                this.position[0]++;
+                break;
+            case 'U':
+                this.position[2]--;
+                break;
+            case 'D':
+                this.position[2]++;
+                break;
+        }
+    }
+
+    turnLeft() {
+        this.directionIndex = (this.directionIndex + 3) % 4;
+    }
+
+
+    turnRight() {
+        this.directionIndex = (this.directionIndex + 1) % 4;
+    }
+
+    turnUp() {
+        this.directionIndex = 4;
+    }
+
+    turnDown() {
+        this.directionIndex = 5;
+    }
+    executeCommands(commands) {
+        for (let i = 0; i < commands.length; i++) {
+            const command = commands[i];
+            if (!['f', 'b', 'l', 'r', 'u', 'd'].includes(command)) {
+                throw new Error(`Invalid command: ${command}`);
+            }
+            switch (command) {
+                case 'f':
+                    this.moveForward();
+                    break;
+                case 'b':
+                    this.moveBackward();
+                    break;
+                case 'l':
+                    this.turnLeft();
+                    break;
+                case 'r':
+                    this.turnRight();
+                    break;
+                case 'u':
+                    this.turnUp();
+                    break;
+                case 'd':
+                    this.turnDown();
+                    break;
+            }
+        }
+    }
 }
+
+module.exports = GalacticSpaceCraftControl;
